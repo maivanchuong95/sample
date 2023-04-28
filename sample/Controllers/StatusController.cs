afadf59afcf45d32a72357f04c019c131e9ed77a
maivanchuong95 <maivanchuong95.it@gmail.com>
2023-04-29 04:26:35 +0700

    Add get status server controller

---
+++ sample/Controllers/StatusController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace sample.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class StatusController : ControllerBase
+    {
+        public StatusController()
+        {
+
+        }
+
+        [HttpGet(Name = "status")]
+        public ActionResult Status()
+        {
+            return Ok(true);
+        }
+    }
+}
