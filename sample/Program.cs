418d80afaef90d1178870c445e9e42efc407203d
maivanchuong95 <maivanchuong95.it@gmail.com>
2023-04-29 04:16:43 +0700

    Show swagger in PROD

--- sample/Program.cs
+++ sample/Program.cs
@@ -16,11 +16,8 @@ namespace sample
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
+            app.UseSwagger();
+            app.UseSwaggerUI();
 
             app.UseHttpsRedirection();
 
